658506e9aa1786f7233662cc16bcb4c09909af06
Tatiana Rosales Hernández <madkodevian@gmail.com>
2021-12-15 23:56:22 +0000

    new commit

--- app/360Tour/Assets/Scripts/User.cs
+++ app/360Tour/Assets/Scripts/User.cs
@@ -2,14 +2,8 @@
 
 public class User
 {
-    public string[] categories;
-    public string id;
-    public string firstName;
-    public string lastName;
-    public string email;
-    public string created_at;
-    public string icon_url;
-    public string updated_at;
-    public string url;
-    public string value;
+    public string id { get; set; }
+    public string firstName { get; set; }
+    public string lastName { get; set; }
+    public string email { get; set; }
 }
